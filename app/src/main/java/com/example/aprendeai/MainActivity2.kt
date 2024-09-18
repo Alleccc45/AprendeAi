096644b1287291d0d4041700fd05abc8276962d2
LCC <aleclohan045@gmail.com>
2024-09-18 15:10:51 -0300

    ok

---
+++ app/src/main/java/com/example/aprendeai/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.aprendeai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aprendeai.ui.main.MainFragment
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fimactivity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
